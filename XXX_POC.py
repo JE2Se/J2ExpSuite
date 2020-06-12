dea876c542594ca7f7f34fd1b512e9ee5aa83041
JE2Se <834590049@qq.com>
2020-06-12 09:33:43 +0800

    再次简化😍😍😍

--- XXX_POC.py
+++ XXX_POC.py
@@ -1,20 +1,17 @@
 # encoding: utf-8
 from lib import *
 import logging
-from lib.Urldeal import urldeal
+from lib.Urldeal import umethod
 
 def XXX_POC(Url): #必须与脚本名称相同
-    scheme, url, port = urldeal(Url)
-    if port is None and scheme == 'https':
-        port = 443
-    elif port is None and scheme == 'http':
-        port = 80
-    else:
-        port = port
-    urldata = scheme + "://" + url + ':' + str(port) + '/login.action'
-    #POC部分，按需更改
+    scheme, url, port = umethod(Url) #URL处理，拆分初协议
+    
+    #-----------POC部分，下，按需更改--------------------
+    urldata = scheme + "://" + url + ':' + str(port) + '/login.action
+    
     try:
         if "漏洞判断成功条件":
+    #-----------POC部分，上，输出部分--------------------
             print(Vcolors.RED +"[!] 存在【漏洞名称】漏洞\r版本号:什么漏洞\r" + Vcolors.ENDC)
     except:
         logging.error("【脚本名称】脚本出现异常")
